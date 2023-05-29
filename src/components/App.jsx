bad205b720851c01c69a761b4c109df718eb976c
Stiroid <stakhovsky89@gmail.com>
2023-05-29 18:11:04 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,50 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
-import Cast from './Cast/Cast';
-import Review from './Reviews/Reviews';
+// import MoviesPage from 'views/Searchpage/MoviesPage';
+
+import Appbar from './AppBar/AppBar';
+// import NotFoundView from 'ui/NotFoundView';
+// import Cast from './Cast/Cast';
+// import HomePage from './HomePage/HomePage';
+// import MovieDetailsPage from './MovieDetailsPage/MovieDetailsPage';
+// import Reviews from './Reviews/Reviews';
+// import SearchBar from './MoviesPage/SearchBar';
 
 const HomePage = lazy(() =>
-  import('../pages/Home/Home')
+  import('./HomePage/HomePage' /* webpackChunkName: "HomePage" */)
 );
-
+// const MoviesPage = lazy(() =>
+//   import('../views/MoviesPage' /* webpackChunkName: "MoviesPage" */)
+// );
 const MoviesPage = lazy(() =>
-  import('../pages/Movies/Movies')
+  import('../views/MoviesPage' /* webpackChunkName: "MoviesPage" */)
 );
-
 const MovieDetailsPage = lazy(() =>
-  import('../pages/MovieDetailsPage/MovieDetailsPage')
+  import(
+    './MovieDetailsPage/MovieDetailsPage' /* webpackChunkName: "MovieDetailsPage" */
+  )
 );
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Cast = lazy(() => import('./Cast/Cast' /* webpackChunkName: "Cast" */));
 
 export const App = () => {
   return (
-    <Suspense fallback="">
-      <Routes>
-        <Route path="/goit-react-hw-05-movies/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/goit-react-hw-05-movies/movies"
-            element={<MoviesPage />}
-          />
-          <Route
-            path="/goit-react-hw-05-movies/movies/:movieId"
-            element={<MovieDetailsPage />}
-          >
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Review />} />
+    <>
+      <Appbar />
+      {/* <SearchBar></SearchBar> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/movies" element={<MoviesPage />}></Route>
+          <Route path="/movies/:movieId/" element={<MovieDetailsPage />}>
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+            <Route path="/movies/:movieId/cast" element={<Cast />} />
           </Route>
-        </Route>
-      </Routes>
-    </Suspense>
+
+          {/* <Route path="*" element={<NotFoundView />} /> */}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
